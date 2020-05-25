6906eeea308a11b0b1629def48ed02974e1a8e81
Pedro <pkupersmit@gmail.com>
2020-05-25 04:17:25 -0300

    Added carousel to keep watching section

--- js/index.js
+++ js/index.js
@@ -1,21 +1,9 @@
-// const leftArrow = document.querySelector(".left-arrow");
-// const rightArrow = document.querySelector(".right-arrow");
-// const series = document.querySelector(".series-carousel");
+const carousels = document.querySelectorAll(".carousels");
 
-
-// rightArrow.addEventListener("click", () => {
-
-//     series.scrollLeft += series.offsetWidth;
-
-// })
-
-// leftArrow.addEventListener("click", () => {
-//     series.scrollLeft -= series.offsetWidth;
-// })
-
-
-const config = {
-    type: 'carousel',
-    perView: 5
-}
-new Glide('.glide', config).mount()
+Object.values(carousels).map(carousel => {
+      const slider = new Glide(carousel, {
+        type: "carousel",
+        perView: 5
+      });
+      slider.mount();
+});
