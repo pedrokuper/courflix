746753c3867c958f646c515d435323f8b9b81a43
Pedro <pkupersmit@gmail.com>
2020-05-25 05:56:36 -0300

    added breakpoints to carousels

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 const carousels = document.querySelectorAll(".carousels");
 
 Object.values(carousels).map(carousel => {
-      const slider = new Glide(carousel, {
-        type: "carousel",
-        perView: 5
-      });
-      slider.mount();
+  const slider = new Glide(carousel, {
+    type: "carousel",
+    perView: 5,
+    gap: 10,
+    breakpoints: {
+      800: {
+        perView: 2
+      },
+      480: {
+        perView: 1
+      }
+    }
+
+  });
+  slider.mount();
 });
