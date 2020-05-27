e74987039f1987df3fb515128de9933bc1f9d683
Pedro <pkupersmit@gmail.com>
2020-05-27 01:52:58 -0300

    Changed some DIR locations and worked on breaking bad section

--- js/index.js
+++ js/index.js
@@ -1,36 +1,23 @@
 const carousels = document.querySelectorAll(".carousels");
 
 Object.values(carousels).map(carousel => {
-  const slider = new Glide(carousel, {
-    type: "carousel",
-    perView: 5,
-    gap: 10,
-    breakpoints: {
-      800: {
-        perView: 2
-      },
-      480: {
-        perView: 1
-      }
-    }
+    const slider = new Glide(carousel, {
+        type: "carousel",
+        perView: 5,
+        gap: 10,
+        
+        breakpoints: {
+            800: {
+                perView: 2
+            },
+            480: {
+                perView: 1
+            }
+        }
 
-  });
-  slider.mount();
+    });
+    
+    
+    slider.mount();
+    
 });
-
-
-
-/*
-Try this code, it will work as your wish.
-
-On 1st Page store data into localStorage variable
-
-var page_content = document.getElementById("parent1").innerHTML;
-console.log( page_content );
-localStorage.setItem("page_content", page_content );
-Retrieve 1st page data on 2nd page
-
-document.getElementById("parent2").innerHTML = localStorage.getItem("page_content");
-console.log( page_content );
-Open console for confirmation data storing successfully on 1st page.
-*/
