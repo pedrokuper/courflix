32c038115206f49e48a7784dca007c2ba6463c44
Pedro <pkupersmit@gmail.com>
2020-06-28 04:04:18 -0300

    Added funcionality to close links after clicking on a section in the burger menu

--- js/index.js
+++ js/index.js
@@ -1,30 +1,71 @@
-$('.carousel-container').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
-  });
-
-  $(document).ready(function(){
-    $('.slider').slick({
-        autoplay:true,
-        
-    });
-});
-
+//DOM
 
 const burgerMenu = document.querySelector(".burger-menu");
 
 const navBar = document.querySelector(".nav-bar");
 
-const displayBurgerMenu = () => {
-    if (navBar.style.display === "block") {
+const navBarLinks = document.querySelectorAll('.link-container')
+
+//CAROUSEL
+$('.carousel-container').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    
+    arrows: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    
+    responsive: [
+        {
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 3,           
+            
+          }
+        },
+        {
+          breakpoint: 780,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+      
+  });
+
+
+
+
+//BURGERMENU
+
+const displayBurgerMenu = (e) => {
+    if (navBar.style.display == "block") {
         navBar.style.display = "none";
     } else {
         navBar.style.display = "block";
     }
+
+    //navBarLinks es un array. Cada vez que se hace click en un elemento del array, se ejecuta el display = none, que cierra el menu.
+    navBarLinks.forEach(element => {
+        element.addEventListener('click', () => {
+            navBar.style.display = 'none';
+        })
+    });
+
+
 
 }
 
