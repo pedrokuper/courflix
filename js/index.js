564f09f44ef4933def2b907618115e93fd943409
Pedro <pkupersmit@gmail.com>
2020-06-27 22:28:49 -0300

    Glidejs replaced for Slick.js

--- js/index.js
+++ js/index.js
@@ -1,50 +1,15 @@
-const carousels = document.querySelectorAll(".carousels");
+$('.carousel-container').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+  });
 
-Object.values(carousels).map(carousel => {
-    const slider = new Glide(carousel, {
-        type: "carousel",
-        perView: 5,
-        gap: 30,
-
-        breakpoints: {
-            800: {
-                perView: 2
-            },
-            320: {
-                perView: 0,
-                gap: 10,
-                
-            },
-            440: {
-                perView: 1,
-                gap: 10,
-            }
-        }
-
+  $(document).ready(function(){
+    $('.slider').slick({
+        autoplay:true,
+        
     });
-
-
-    
-
-
-    slider.mount();
-    
 });
-
-
-
-
-const burgerMenu = document.querySelector(".burger-menu");
-
-const navBar = document.querySelector(".nav-bar");
-
-const displayBurgerMenu = () => {
-    if (navBar.style.display === "block") {
-        navBar.style.display = "none";
-    } else {
-        navBar.style.display = "block";
-    }
-
-}
-
-burgerMenu.onclick = displayBurgerMenu;
